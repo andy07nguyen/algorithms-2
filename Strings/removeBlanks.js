49c8b5b7b3f28a4c368c3d0e0f3350c6ac2ea426
Nishant Patel <contact@nishant-patel.com>
2017-05-18 21:46:50 -0700

    Auto Push by Nishant Patel

---
+++ Strings/removeBlanks.js
@@ -0,0 +1,15 @@
+// ============================================================
+// Given a string, return all string contents, but without blanks.
+// ============================================================
+
+String.prototype.removeBlanks = function () {
+    for (var i = 0; i < this.length; i++) {
+        if (this[i] === " ") {
+
+        }
+    }
+};
+
+a = "This is Fun"
+a.removeBlanks()
+console.log(a);
